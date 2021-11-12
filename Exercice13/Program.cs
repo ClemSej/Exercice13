889e88ba4d26d04253420ec8c7358911e9cb93e7
User <User@DESKTOP-6SOIADA>
2021-11-12 15:25:24 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //déclaration
+            int entier;
+
+            //boucle de demande de saisie
+
+            do
+            {
+                Console.WriteLine("Veuillez saisir un entier entre 1 et 9: ");
+                entier = int.Parse(Console.ReadLine());
+            } while (entier <1 || entier >9);
+            //boucle sur la table
+
+            for (int k = 0; k <= 10; k++)
+            {
+                Console.WriteLine(entier + " x " + k + " = " + (entier * k));
+
+            }
+                                 
+            Console.ReadLine();
+
+        }
+    }
+}
